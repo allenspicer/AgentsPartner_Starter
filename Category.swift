0c28d4ed028a1e8836cf4b47b1af4a12ef90a74e
Allen Spicer <allenspicer@gmail.com>
2016-07-05 19:50:20 -0400

    Class additions

---
+++ Category.swift
@@ -0,0 +1,15 @@
+//
+//  Category.swift
+//  Agents Partner
+//
+//  Created by Allen Spicer on 7/5/16.
+//  Copyright © 2016 Razeware LLC. All rights reserved.
+//
+
+import Foundation
+import RealmSwift
+
+class Category: Object{
+    dynamic var name = ""
+    
+}
